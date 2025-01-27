a4e12ca335a125b26af05c7e411ab038f1ddf98e
KairviLodhiya <kairvi12@gmail.com>
2025-01-27 15:02:01 -0500

    Removed Demo_lecture4 after moving to class_work branch

--- Demo_lecture4/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "mathlibrary.h"
-
-int main(int argc, char**argv){
-    int number = argc;
-    double sqrtnumber = mysqrt(number);
-    double sq_sqrtnumber = square(sqrtnumber);
-    std::cout<<"answer "<<number<<sqrtnumber<<sq_sqrtnumber;
-}
--- Demo_lecture4/mathlibrary/mathlibrary.cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-
-double mysqrt(double value){
-    return sqrt(value);
-}
-
-double square(double value){
-    return value*value;
-}
