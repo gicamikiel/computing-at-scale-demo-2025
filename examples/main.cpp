500fe4c4b62a9e8a889f706ed7a77c0554cca7ed
Jacob Merson <jacob@jacobmerson.com>
2025-02-02 11:35:57 -0500

    add move vector examples

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,10 +6,19 @@ int main(int argc, char** argv) {
   for(int i=0; i<v.size(); ++i){
     v[i] = i;
   }
-  Vector v2(v);
+  //Vector v2(v);
+  Vector v2(10);
+  {
+    Vector v3(100);
+    for(int i=0; i<v3.size(); ++i){
+      v3[i] = i;
+    }
+    v2 = std::move(v3);
+    std::cout<<"V3 size: "<<v3.size()<<"\n";
+  }
 
-  for(int i=0; i<v.size(); ++i){
-    std::cout<<"("<<v[i]<<","<<v2[i]<<") ";
+  for(int i=0; i<v2.size(); ++i){
+    std::cout<<"("<<v2[i]<<") ";
   }
   std::cout<<"\n";
 
--- examples/vector.hpp
+++ examples/vector.hpp
@@ -4,22 +4,55 @@
 class Vector {
   public:
 
-  Vector() : data_(nullptr), size_(0) {}
+  Vector() : data_(nullptr), size_(0) {
+    std::cout<<"default constructor\n";
+  }
   explicit Vector(int size) {
+    std::cout<<"constructor with size\n";
     size_ = size;
     data_ = new double[size];
   }
 
   // copy constructor
-  Vector(const Vector * v) : data_(new double[v.size()]), size_(v.size())  {
+  Vector(const Vector & v) : data_(new double[v.size()]), size_(v.size())  {
     std::cout<<"Calling copy constructor\n";
     assert(v.size() == size_);
-    for(int i=0; i< v.size(); ++i){
-      data_[i] = v[i];
-    }
+    std::copy(&(v.data_[0]), &(v.data_[size_]), &(data_[0]));
   }
 
+  /*
+  Vector& operator=(Vector other) {
+    std::cout<<"copy assignment\n";
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    return *this;
+  }
+  */
+
+  Vector& operator=(const Vector& other){
+    std::cout<<"copy assignment\n";
+    Vector tmp(other);
+    std::swap(data_, tmp.data_);
+    std::swap(size_, tmp.size_);
+    return *this;
+  }
+
+  // move constructor
+  Vector(Vector&& other){
+    std::cout<<"move constructor";
+    std::swap(size_, other.size_);
+    std::swap(data_, other.data_);
+  }
+  Vector& operator=(Vector&& other) {
+    std::cout<<"move assignment\n";
+    std::swap(size_, other.size_);
+    std::swap(data_, other.data_);
+    return *this;
+  }
+
+
   ~Vector() noexcept {
+    std::cout<<"destructor\n";
     delete data_;
   }
 
