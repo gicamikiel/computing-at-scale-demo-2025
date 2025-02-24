f50a24e12c5df0a4a0bf3ff5bc1b76fdc83dabcb
Mikiel Gica <gicam@rpi.edu>
2025-02-24 14:47:38 -0500

    try github actions

---
+++ examples/install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/catchorg/Catch2 ../Catch2
+cmake -S ../Catch2 -B ../build/Catch2 \
+    -DCMAKE_INSTALL_PREFIX=../build-Catch2/install/
+cmake --build ../build/Catch2 -j$(nproc) --target install
+
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:../build-Catch2/install
+cmake . -B $(pwd)/build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=g++
